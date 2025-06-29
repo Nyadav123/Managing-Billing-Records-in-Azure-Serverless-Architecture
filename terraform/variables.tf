c3f62843c3d3d13c58c6d1203d358fb71f933973
NIPUN YADAV <89506943+Nyadav123@users.noreply.github.com>
2025-06-29 22:55:42 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" {
+  default = "cosmosdb-optimization-rg"
+}
+
+variable "location" {
+  default = "East US"
+}
+
+variable "storage_account_name" {
+  default = "cosmosdbarchivestorage"
+}
+
+variable "cosmosdb_account_name" {
+  default = "cosmosdboptacct"
+}
